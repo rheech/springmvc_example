cc7f89c5c90024e77fa169e6eabb7318edb7ccd3
Cheonghyun Lee <lee@cheonghyun.com>
2018-06-25 15:58:59 +0900

    Add products page (p.57)

--- src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
+++ src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
@@ -23,7 +23,7 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
     public InternalResourceViewResolver getInternalResourceViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class);;
-        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
 
         return resolver;
